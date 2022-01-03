8af690ebf97319dd09b4a559197020456849e90b
mickeytheone <mickeyfeng15@gmail.com>
2022-01-03 00:04:26 -0600

    Updates

---
+++ precache-manifest.54b25e179a18fa96e3f1fc64cb047184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3817e458c73b7c7ff0770229d58b7d97",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "d4c9bee26179b0a359de",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "610bdc9597ae3714515e",
+    "url": "/box-office/static/js/2.f0dcf50e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.f0dcf50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c9bee26179b0a359de",
+    "url": "/box-office/static/js/main.a6b8f44c.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
